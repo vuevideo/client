096d9d5c11593f15ba311f60a89eee8fa652ec5c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-05-15 22:06:35 +0530

    UI Packages added with validation.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      proxyUrl: process.env.NUXT_PUBLIC_PROXY_URL || 'http://localhost:3001',
+      proxyUrl: process.env.NUXT_PUBLIC_PROXY_URL,
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGEING_SENDER_ID,
-      appId: process.env.appId,
+      appId: process.env.APP_ID,
+    },
+  },
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
     },
   },
 });
