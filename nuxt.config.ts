98b98453851eee75ab302ae2c05e73b38fe0aea1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-05-22 21:09:01 +0530

    Frontend Firebase Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      proxyUrl: process.env.NUXT_PUBLIC_PROXY_URL,
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGEING_SENDER_ID,
-      appId: process.env.APP_ID,
+      proxyUrl: process.env.NUXT_PUBLIC_PUBLIC_PROXY_URL,
+      apiKey: process.env.NUXT_PUBLIC_API_KEY,
+      authDomain: process.env.NUXT_PUBLIC_AUTH_DOMAIN,
+      projectId: process.env.NUXT_PUBLIC_PROJECT_ID,
+      storageBucket: process.env.NUXT_PUBLIC_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_PUBLIC_MESSAGING_SENDER_ID,
+      appId: process.env.NUXT_PUBLIC_APP_ID,
     },
   },
   css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
