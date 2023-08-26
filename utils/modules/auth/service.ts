e4fe44bebb78216ad8a6ce3e492b1dde858fcfbe
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-08-26 16:50:20 +0530

    Comments

--- utils/modules/auth/service.ts
+++ utils/modules/auth/service.ts
@@ -5,12 +5,18 @@ import { LoginAccountDto } from './dtos/login-account.dto';
 import { useFirebase } from '~/composables/useFirebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
+/**
+ * Service Implementation for creating a new user account on server.
+ * @param createAccountDto DTO Implementation for creating new account on server.
+ * @returns Newly created Credentials or Error Details.
+ */
 export const registerNewAccount = async (
   createAccountDto: CreateAccountDto
 ): Promise<{
   error: HttpException | null;
   data: Credentials | null;
 }> => {
+  // Sending POST request to create a new user account.
   const { data, error } = await useFetch('/api/v1/auth', {
     method: 'POST',
     headers: {
@@ -19,6 +25,7 @@ export const registerNewAccount = async (
     body: JSON.stringify(createAccountDto.toJson()),
   });
 
+  // Return response data or error details.
   return {
     data: data.value ? Credentials.fromJson(data.value) : Credentials.empty(),
     error: error.value
@@ -27,6 +34,11 @@ export const registerNewAccount = async (
   };
 };
 
+/**
+ * Service Implementation to log into the user account.
+ * @param loginAccountDto DTO Implementation to log into the user account.
+ * @returns Error Details if any.
+ */
 export const loginAccount = async (
   loginAccountDto: LoginAccountDto
 ): Promise<{
@@ -36,17 +48,20 @@ export const loginAccount = async (
   try {
     const { auth } = useFirebase();
 
+    // Sign in using email address and password using Firebase Auth
     await signInWithEmailAndPassword(
       auth,
       loginAccountDto.emailAddress,
       loginAccountDto.password
     );
 
+    // Return success
     return {
       data: null,
       error: HttpException.empty(),
     };
   } catch (error: any) {
+    // Handle Firebase errors.
     let errorString = 'Something went wrong, please try again later';
 
     if (error.code === 'auth/user-not-found') {
