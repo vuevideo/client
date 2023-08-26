0ea4720ec39dac403927c69fe6ee4788c905132f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-08-26 17:17:44 +0530

    Service Implementation for updating email address on server and firebase.

--- utils/modules/user/service.ts
+++ utils/modules/user/service.ts
@@ -1,6 +1,17 @@
 import { HttpException } from '~/utils/models/http-exception.model';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Account } from '~/utils/models/account.model';
+import { UpdateEmailDto } from './dtos/update-email.dto';
+import { Credentials } from '~/utils/models/credentials.model';
+import {
+  getIdToken,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+  updateEmail,
+} from 'firebase/auth';
+import { useFirebase } from '~/composables/useFirebase';
+
+const { auth } = useFirebase();
 
 /**
  * Service Implementation for updating user details on server.
@@ -13,11 +24,15 @@ export const updateUser = async (
   error: HttpException | null;
   data: Account | null;
 }> => {
+  // Generating a Firebase JWT token.
+  const firebaseJwt = getIdToken(auth.currentUser!);
+
   // Send PUT request to update user details on server.
   const { data, error } = await useFetch('/api/v1/user', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${firebaseJwt}`,
     },
     body: JSON.stringify(updateUserDto.toJson()),
   });
@@ -29,5 +44,90 @@ export const updateUser = async (
       ? HttpException.fromJson(error.value!.data)
       : HttpException.empty(),
   };
+};
+
+/**
+ * Service Implementation for updating email address on server.
+ * @param updateUserDto DTO Implementation for updating email address.
+ * @returns Updated Credentials or Error Details.
+ */
+export const updateEmailAddress = async (
+  updateEmailDto: UpdateEmailDto
+): Promise<{
+  error: HttpException | null;
+  data: Credentials | null;
+}> => {
+  // Generating a Firebase JWT token.
+  const firebaseJwt = getIdToken(auth.currentUser!);
+
+  // Send PUT request to update user details on server.
+  const { data, error } = await useFetch('/api/v1/user/email', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${firebaseJwt}`,
+    },
+    body: JSON.stringify(updateEmailDto.toJson()),
+  });
+
+  // Return response data or error details.
+  return {
+    data: data.value ? Credentials.fromJson(data.value) : Credentials.empty(),
+    error: error.value
+      ? HttpException.fromJson(error.value!.data)
+      : HttpException.empty(),
+  };
+};
+
+/**
+ * Service Implementation for updating email address on server and firebase.
+ * @param updateUserDto DTO Implementation for updating email address.
+ * @returns Updated Credentials or Error Details.
+ */
+const updateEmailAddressOnFirebase = async (
+  password: string,
+  updateEmailDto: UpdateEmailDto
+): Promise<{
+  error: HttpException | null;
+  data: Credentials | null;
+}> => {
+  try {
+    // Preparing credentials for reauthentication.
+    const user = auth.currentUser;
+    const credentials = EmailAuthProvider.credential(user!.email!, password);
+
+    // Reauthenticating with firebase.
+    await reauthenticateWithCredential(user!, credentials);
+
+    // Updating email address on firebase.
+    await updateEmail(user!, updateEmailDto.emailAddress);
+
+    // Updating email address on server.
+    return await updateEmailAddress(updateEmailDto);
+  } catch (error: any) {
+    // Handle Firebase errors.
+    let errorString = 'Something went wrong, please try again later';
+
+    if (error.code === 'auth/email-already-in-use') {
+      errorString = 'Email provided is already in use.';
+    }
+
+    if (error.code === 'auth/user-not-found') {
+      errorString = 'This email is not registered to any account.';
+    }
+
+    if (error.code === 'auth/wrong-password') {
+      errorString = 'You have typed a wrong password, please try again.';
+    }
+
+    return {
+      data: null,
+      error: HttpException.fromJson({
+        statusCode: 400,
+        message: errorString,
+        error: 'Bad Request',
+      }),
+    };
+  }
 };
 
